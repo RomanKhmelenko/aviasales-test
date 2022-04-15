761d4a0779fa4e4fa3b09d6e622c19c15017a327
RomanKhmelenko <xmelenko1992@gmail.com>
2022-04-15 12:46:43 +0300

    slice

--- src/components/Tickets/Tickets.js
+++ src/components/Tickets/Tickets.js
@@ -1,23 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Ticket from "./Ticket";
 import { fetchTickets } from "../../redux/actions";
 
 const Tickets = () => {
     const dispatch = useDispatch()
-    const tickets = useSelector((state) => state.tickets);
+    const allTickets = useSelector((state) => state.tickets);
 
     useEffect(() => {
         dispatch(fetchTickets())
       }, [dispatch]);
     
+    const [ticketsQuantity, setTicketsQuantity] = useState(2)
+
+    const ticketsToShow = allTickets.slice(0, ticketsQuantity);
+
+    allTickets && console.log(allTickets);
+    console.log(ticketsToShow);
+
     return (
         <div>
             <div>
-                {tickets.map((ticket, index) => {
-                    if (index <= 4) {
+                {ticketsToShow.map((ticket) => {
                         return <Ticket ticket={ticket} key={ticket.price} />
-                    }
                 })}
             </div>
         </div>
